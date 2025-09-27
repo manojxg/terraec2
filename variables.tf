e4dd42d2da4874aad88b25974bb89c696d543748
manojxg <87294078+manojxg@users.noreply.github.com>
2025-09-27 12:25:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,19 @@
 variable "region" {
-  default = "eu-west-1"
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
 }
 
 variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "ami_id" {
-  default = "ami-0bc691261a82b32bc"
+variable "key_name" {
+  default = "my-key" # must exist in AWS
+}
+
+variable "aws_profile" {
+  default = "myprofile"  # 👈 AWS CLI profile name
 }
