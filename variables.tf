6c1d27e56166b57ecbc1d482ae3887766ddcf8cf
manojxg <87294078+manojxg@users.noreply.github.com>
2025-09-27 12:30:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-  default = "us-east-1"
+  default = "eu-west-1"
 }
 
 variable "ami_id" {
-  default = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+  default = "ami-0bc691261a82b32bc" # Amazon Linux 2 AMI
 }
 
 variable "instance_type" {
@@ -11,7 +11,7 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  default = "my-key" # must exist in AWS
+  default = "Manoj-test" # must exist in AWS
 }
 
 variable "aws_profile" {
