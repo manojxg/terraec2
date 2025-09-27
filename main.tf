989711e7820902d34b0632d72e3933d27aa96ad0
manojxg <87294078+manojxg@users.noreply.github.com>
2025-09-27 12:34:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id                  # 👈 Specific Subnet
+  vpc_security_group_ids      = [var.security_group_id]         # 👈 Security Group
 
   tags = {
     Name = "jenkins-profile-ec2"
