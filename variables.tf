4586282579c434dbc0abd1755c09e828be34aec0
manojxg <87294078+manojxg@users.noreply.github.com>
2025-10-06 09:21:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,9 @@ variable "region" {
 }
 
 variable "ami_id" {
-  default = "ami-0bc691261a82b32bc"
+  default = "ami-0bc691261a82b32bc" # Amazon Linux 2
 }
 
-variable "ROLE_ARN" {
-  description = "IAM role ARN to assume"
-}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "aws_session_token" {}
