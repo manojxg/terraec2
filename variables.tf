684261b64fc7cf641bfc548107fd947ac86bbdfd
manojxg <87294078+manojxg@users.noreply.github.com>
2025-10-06 09:09:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,11 @@
 variable "region" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "ami_id" {
-  default = "ami-0bc691261a82b32bc" # Amazon Linux 2 AMI
+  default = "ami-0c02fb55956c7d316"
 }
 
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  default = "Manoj-test" # must exist in AWS
-}
-
-variable "aws_profile" {
-  default = "myprofile"  # 👈 AWS CLI profile name
-}
-
-variable "subnet_id" {
-  description = "The subnet ID where EC2 should be launched"
-  default     = "subnet-0e71381a11f27c2c4" # replace with your subnet
-}
-
-variable "security_group_id" {
-  description = "Security group for EC2"
-  default     = "sg-0cf62ab6398dbaba9" # replace with your SG
+variable "ROLE_ARN" {
+  description = "IAM role ARN to assume"
 }
